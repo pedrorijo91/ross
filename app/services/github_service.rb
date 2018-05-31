6ed8577b943c4ac039f65e78a96dcb65a2d33fb9
pedrorijo91 <pedrorijo91@gmail.com>
2018-05-31 22:18:07 +0100

    Extract github cache

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,5 +1,39 @@
 class GithubService
 
+  class Cache
+    private_class_method def self.cache_key_user(username)
+      "github_cache_key/#{username}"
+    end
+
+    private_class_method def self.cache_key_forks(username)
+      "github_cache_key_forks/#{username}"
+    end
+
+    private_class_method def self.read(key)
+      Rails.cache.read(key)
+    end
+
+    private_class_method def self.write(key, value)
+      Rails.cache.write(key, value, expires_in: 1.hours)
+    end
+
+    def self.write_nbr_forks(username, nbr_forks)
+      write(cache_key_forks(username), nbr_forks)
+    end
+
+    def self.read_nbr_forks(username)
+      read(cache_key_forks(username))
+    end
+
+    def self.read_or_write_user_stats(username)
+      Rails.cache.fetch(cache_key_user(username), expires_in: 1.hours) do
+        yield
+      end
+    end
+  end
+
+  private_constant :Cache
+
   def initialize
     @client = Octokit::Client.new
     @client.auto_paginate = true
@@ -18,33 +52,25 @@ class GithubService
   end
 
   private def fetch_repo_stats(username)
-    puts "Computing repo stats for #{username}"
-
     public_repos = fetch_public_repos(username)
     non_forks = public_repos.select { |repo| !repo.fork }
 
     forks = public_repos.select { |repo| repo.fork }
-    Rails.cache.write(cache_key_forks(username), forks.size)
+    Cache.write_nbr_forks(username, forks.size)
 
     non_forks.map { |repo| RepoStats.new(repo.id, repo.name, repo.html_url, repo.stargazers_count, repo.forks_count, repo.language)}
   end
 
-  private def cache_key_user(username)
-    "github_cache_key/#{username}"
-  end
-
-  private def cache_key_forks(username)
-    "github_cache_key_forks/#{username}"
-  end
-
   def fetch_user_stats(username)
-    Rails.cache.fetch(cache_key_user(username), expires_in: 1.hours) do
+    Cache.read_or_write_user_stats(username) do
+      puts "Computing user stats for #{username}"
       repo_stats = fetch_repo_stats(username)
       nbr_stared = fetch_nbr_stared(username)
       user = fetch_user(username)
-      nbr_forks = Rails.cache.read(cache_key_forks(username))
+      nbr_forks = Cache.read_nbr_forks(username)
 
       UserStats.new(user.id, username, user.html_url, user.avatar_url, repo_stats, nbr_stared, nbr_forks)
     end
+
   end
 end
