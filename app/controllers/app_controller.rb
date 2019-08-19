39881b7e45b99528f5c5f868a1162c2bf3271b7d
pedrorijo91 <pedrorijo91@gmail.com>
2019-08-19 19:56:18 +0100

    Fix forced url param

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -12,7 +12,8 @@ class AppController < ApplicationController
     username = params[:username]
     puts "USERNAME: #{username}" # FIXME logger
 
-    stats = @github.fetch_user_stats(username)
+    forced = if params['forced'] then true else false end
+    stats = @github.fetch_user_stats(username, forced)
 
     render locals: {stats: stats}
   end
