f47bc6aa3fad498f2abac31a74bd1b166af50e1d
pedrorijo91 <pedrorijo91@gmail.com>
2019-09-22 22:36:27 +0100

    Add validation if username is sent

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -8,12 +8,18 @@ class AppController < ApplicationController
   def index; end
 
   def form_post
-    redirect_to user_stats_path(params[:username])
+    if params[:username].blank?
+      Rails.logger.warn 'No username received on request'
+      redirect_to home_path, notice: 'Missing username'
+    else
+      redirect_to user_stats_path(params[:username])
+    end
   end
 
   def stats
     username = params[:username]
     forced = params['forced'] ? true : false
+
     Rails.logger.debug "Stats for #{username} (forced=#{forced})"
 
     stats = @github.fetch_user_stats(username, forced)
